2d1eca077621a423a45135c63510bdd4510da0a0
Midou <midou.ik.1999@gmail.com>
2024-10-13 22:59:48 +0200

    'return to git'

---
+++ app.R
@@ -0,0 +1,22 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny) 
+library(readr)
+library(shinyWidgets)
+library(shiny) 
+library(plotly) 
+library(dplyr)
+
+source('server.R')
+source('ui.R')
+
+# Run the application 
+shinyApp(ui = ui, server = server)
